20dafe6ca2c1a841bd2c05b06bdaa9354a964e13
dtxpder <1119753960@qq.com>
2025-03-20 01:52:52 -0400

    update docker file

--- project/Start.sh
+++ project/Start.sh
@@ -1,27 +1,38 @@
+#set -e
+#
+#echo "Starting Docker containers..."
+#docker-compose up -d
+#sleep 5
+#echo "Copying JSON data into MongoDB container..."
+#docker cp data/us-states-array.json mongodb:/us-states-array.json
+#docker exec mongodb mongoimport --db usmap --collection states --file /us-states-array.json --jsonArray
+#
+#echo "Installing backend dependencies..."
+#cd backend && npm install
+#cd ..
+#
+#echo "Installing frontend dependencies..."
+#cd frontend && npm install
+#cd ..
+#
+#echo "Starting backend server..."
+#cd backend
+#npm run start &
+#
+#echo "Starting frontend server..."
+#cd ../frontend
+#npm run dev &
+#
+#echo "All services started successfully"
+#wait
+
+
+#!/bin/bash
 set -e
 
 echo "Starting Docker containers..."
-docker-compose up -d
+docker-compose up --build -d
 
-echo "Copying JSON data into MongoDB container..."
-docker cp data/us-states-array.json mongodb:/us-states-array.json
-docker exec mongodb mongoimport --db usmap --collection states --file /us-states-array.json --jsonArray
-
-echo "Installing backend dependencies..."
-cd backend && npm install
-cd ..
-
-echo "Installing frontend dependencies..."
-cd frontend && npm install
-cd ..
-
-echo "Starting backend server..."
-cd backend
-npm run start &
-
-echo "Starting frontend server..."
-cd ../frontend
-npm run dev &
-
-echo "All services started successfully"
-wait
+echo "All services started successfully!"
+echo "Frontend running at: http://localhost:5173"
+echo "Backend running at: http://localhost:4000/graphql"
