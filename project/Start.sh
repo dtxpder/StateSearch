261183bb9428d5841f3daf0f6ec42e3dfda9770b
dtxpder <1119753960@qq.com>
2025-03-20 01:05:16 -0400

    copy data

--- project/Start.sh
+++ project/Start.sh
@@ -3,6 +3,10 @@ set -e
 echo "Starting Docker containers..."
 docker-compose up -d
 
+echo "Copying JSON data into MongoDB container..."
+docker cp data/us-states-array.json mongodb:/us-states-array.json
+docker exec mongodb mongoimport --db usmap --collection states --file /us-states-array.json --jsonArray
+
 echo "Installing backend dependencies..."
 cd backend && npm install
 cd ..
@@ -20,7 +24,4 @@ cd ../frontend
 npm run dev &
 
 echo "All services started successfully"
-echo "Frontend running at: http://localhost:5173"
-echo "Backend running at: http://localhost:4000/graphql"
-
 wait
